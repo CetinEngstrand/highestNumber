cdfa9e6f5a24d6f3710749f312506bcb8717a567
CetinEngstrand <cetin.engstrand@gmail.com>
2012-11-15 11:26:05 +0100

    Change Comment

--- highestNumber/src/highestNum/mainClass.java
+++ highestNumber/src/highestNum/mainClass.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class mainClass {
 
 	/**
-	 * @param args
+	 * Change Comment
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
